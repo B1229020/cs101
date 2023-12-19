26f5d6f56b404577961a5afc9009bc08dfbdfb8f
B1229020 <144936822+B1229020@users.noreply.github.com>
2023-12-19 23:58:24 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+int main(){
+    int n = 7;
+    int spc = n;
+    for(int i = 1; i <= n; i++){
+        printf("%*d", spc, i);
+        for(int j = 2; j <= i; j++){
+            printf(" %d", i);
+        }
+        printf("\n");
+        spc--;
+    }
+}
