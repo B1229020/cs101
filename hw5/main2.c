4d417a6b4e1770daf75a9f09af7357487516fa76
B1229020 <144936822+B1229020@users.noreply.github.com>
2023-12-20 20:54:12 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
 int main(){
-    double n = 4.0;
     double PI = 4.0;
     int j = 1;
-    for(int i = 1; PI >= 3.14159; i++){
+    for(int i = 1; PI >= 3.1416 || PI < 3.14159; i++){
         j += 2;
-        PI = PI - n/j;
-        if(PI-3.14159 < 0.00001 && PI-3.14159 > 0){
-            break;
+        if(i%2){
+            PI = PI - 4.0/j;
         }
-        else if(PI-3.14159 > -0.00001 && PI-3.14159 < 0){
-            break;
-        }
-        j += 2;
-        PI = PI + n/j;
-        if(PI-3.14159 < 0.00001 && PI-3.14159 > 0){
-            break;
-        }
-        else if(PI-3.14159 > -0.00001 && PI-3.14159 < 0){
-            break;
+        else{
+            PI = PI + 4.0/j;
         }
     }
     printf("%d", j);
